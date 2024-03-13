bb174e15fdbfce1d533aaf3351cea9e46634f896
Cristian Calina <cristian.calina@uipath.com>
2024-03-12 21:12:31 -0700

    chore: use single-spa navigation

--- root-config/src/cristian-root-config.ts
+++ root-config/src/cristian-root-config.ts
@@ -1,5 +1,6 @@
 import {
     LifeCycles,
+    navigateToUrl,
     registerApplication,
     start,
 } from 'single-spa';
@@ -11,7 +12,14 @@ Object.entries(manifest).forEach(([appName, appRoute]) => {
         name: appName,
         app: () => System.import(appName) as Promise<LifeCycles>,
         activeWhen: appRoute
-    })
+    });
 });
+
+document.querySelectorAll('#nav a').forEach(a => {
+    a.addEventListener('click', (e) => {
+        e.preventDefault();
+        navigateToUrl(a.getAttribute('href'));
+    });
+})
 
 start();
